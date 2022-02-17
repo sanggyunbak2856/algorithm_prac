ba6d445a0527291fe8fb893fe95752a7ce4f3459
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-02-17 19:51:45 +0900

    heap

--- baekjun/1920.py
+++ baekjun/1920.py
@@ -1,19 +1,51 @@
 import sys
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+class BST:
+    def __init__(self, value):
+        self.head = Node(value)
+    def insert(self, value):
+        self.currentNode = self.head
+        while True:
+            if self.currentNode.value > value:
+                if self.currentNode.left != None:
+                    self.currentNode = self.currentNode.left
+                else:
+                    self.currentNode.left = Node(value)
+                    break
+            else:
+                if self.currentNode.right != None:
+                    self.currentNode = self.currentNode.right
+                else:
+                    self.currentNode.right = Node(value)
+                    break
+    def search(self, value):
+        self.currentNode = self.head
+        while self.currentNode:
+            if self.currentNode.value == value:
+                return True
+            elif self.currentNode.value > value:
+                self.currentNode = self.currentNode.left
+            else:
+                self.currentNode = self.currentNode.right
+        return False
 
 numberN = int(sys.stdin.readline())
 numberNList = sys.stdin.readline()
 numberNList = numberNList.split('\n')[0].split(' ')
+bst = BST(numberNList[0])
+for i in range(1, len(numberNList)):
+    BST.insert(1)
 
 numberM = int(sys.stdin.readline())
 numberMList = sys.stdin.readline()
 numberMList = numberMList.split('\n')[0].split(' ')
 
-for i in numberMList:
-    included = False
-    for j in numberNList:
-        if i == j:
-            included = True
-    if included:
+for j in numberMList:
+    if BST.search(numberMList[j]):
         print(1)
     else:
         print(0)
