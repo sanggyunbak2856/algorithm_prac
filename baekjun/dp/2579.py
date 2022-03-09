9f542669f05d03d305f8c0f7d5ba6568697e5d65
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-03-09 15:30:05 +0900

    dp

---
+++ baekjun/dp/2579.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = [0] * (n + 1)
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    array[i] = int(sys.stdin.readline())
+    if i == 1:
+        dp[1] = array[1]
+    elif i == 2:
+        dp[2] = array[1] + array[2]
+    else:
+        dp[i] = max(dp[i - 2] + array[i], dp[i - 3] + array[i - 1] + array[i])
+
+print(dp[n])
